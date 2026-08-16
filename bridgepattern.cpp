cf1048b24481afbf2222fe03e968e503b1fb39a2
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-08-16 16:01:30 +0530

    bridge,diamond/hollow diamond pattern

---
+++ bridgepattern.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=1; i<=2*n-1; i++){
+        cout<<"* ";
+    }
+    cout<<endl;
+    n--;
+    for(int i=1; i<=n-1; i++){
+        for(int j=1; j<=n+1-i; j++){
+            cout<<"* ";
+        }
+        for(int j=1; j<=2*i-1; j++){
+            cout<<"  ";
+        }
+        for(int j=1; j<=n+1-i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+       
+    }
+    return 0;
+}
+
+   
+        
